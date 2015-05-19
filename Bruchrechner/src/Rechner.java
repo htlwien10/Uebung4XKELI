18ffb4b6c20c5c4950b742c52d42260133ee15ed
Franz <Franz@FK-Desktop.local>
2015-05-19 14:47:40 +0200

    Titel eingefügt

--- Bruchrechner/src/Rechner.java
+++ Bruchrechner/src/Rechner.java
@@ -1,9 +1,30 @@
-
+import AlgoTools.IO;
 public class Rechner {
-
+	
 	public static void main(String[] args) {
-		System.out.println("Test");
-
+			
+		titel("Bruchrechner");
+			
+			
 	}
 
+
+
+
+	static void titel(String text) {
+			
+		IO.print("\n\t\t");
+		
+		for (int i=0; i<text.length()+6; i++)
+			IO.print("*");
+		
+		IO.print("\n\t\t*  "+ text + "  *\n\t\t");
+		
+		for (int i=0; i<text.length()+6; i++)
+			IO.print("*");
+		
+		IO.println("\n");
+	}
+
+		
 }
